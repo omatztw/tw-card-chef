35a229a0f63c6ab51aa8c62bf1816ec7451a549a
おまつ先生 <33661309+omatztw@users.noreply.github.com>
2021-07-18 18:26:44 +0900

    Update

--- src/app/components/card/card.component.spec.ts
+++ src/app/components/card/card.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { CardComponent } from './card.component';
 import { ResultComponent } from '../result/result.component';
@@ -70,7 +70,7 @@ describe('CardComponent', () => {
   };
 
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [CardComponent, ResultComponent, FooterComponent],
       imports: [FormsModule, ReactiveFormsModule],
